16d07fa3ac049a689fc46dab1843d3f7586b97eb
Kristin J <56851408+KayjePrice@users.noreply.github.com>
2020-04-09 20:29:58 -0400

    Scaffoled InitialMigration and UpdatedDatabase

---
+++ web/Migrations/20200402233915_InitialMigration.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace web.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hashers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    HashName = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hashers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trails", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HasherTrail",
+                columns: table => new
+                {
+                    TrailId = table.Column<Guid>(nullable: false),
+                    HasherId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HasherTrail", x => new { x.HasherId, x.TrailId });
+                    table.ForeignKey(
+                        name: "FK_HasherTrail_Hashers_HasherId",
+                        column: x => x.HasherId,
+                        principalTable: "Hashers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HasherTrail_Trails_TrailId",
+                        column: x => x.TrailId,
+                        principalTable: "Trails",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HasherTrail_TrailId",
+                table: "HasherTrail",
+                column: "TrailId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HasherTrail");
+
+            migrationBuilder.DropTable(
+                name: "Hashers");
+
+            migrationBuilder.DropTable(
+                name: "Trails");
+        }
+    }
+}
--- web/Models/Hasher.cs
+++ web/Models/Hasher.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace web.Models 
 {     
@@ -16,10 +17,13 @@ namespace web.Models
 
     public class HasherTrail
     {
+        [ForeignKey ("HasherId")]
         public Hasher Hasher {get; set;}
         public Guid TrailId {get; set;}
         public Guid HasherId { get; set; }
+        [ForeignKey ("TrailId")]
         public Trail Trail {get; set;}
+
     }
 
     public class Trail
